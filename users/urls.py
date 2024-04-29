3237ad65b7590355308f6c593ef8d38f3c8f703b
Nikita Turda <turda.nikita.bsn@gmail.com>
2024-04-29 15:59:05 +0300

    add users login

--- users/urls.py
+++ users/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
+    path('login_user', views.login_user, name='login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,1 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST["userEmail"]
+        password = request.POST["userPassword"]
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('vacancies')
+        else:
+            messages.success(request, "There was an error logging in, try again!")
+            return redirect('login')
+    else:
+        return render(request, 'users/login.html')
