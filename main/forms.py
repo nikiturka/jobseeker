0b05a2c7c7a7d469b2a211f28d3645eb9bf18832
Nikita Turda <turda.nikita.bsn@gmail.com>
2024-04-29 18:58:52 +0300

    form styles

--- main/forms.py
+++ main/forms.py
@@ -4,6 +4,11 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
 
     class Meta:
         model = CustomUser
