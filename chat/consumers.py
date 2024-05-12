e53ec79acd08cf61c1b374a3ee9c4d76f98701e8
Nikita Turda <turda.nikita.bsn@gmail.com>
2024-05-12 11:36:14 +0300

    chat send messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -20,3 +20,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+        message = data['message']
+        user_email = data['user']
+        room = data['room']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': user_email,
+                'room': room
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        user_email = event['user']
+        room = event['room']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'user': user_email,
+            'room': room
+        }))
